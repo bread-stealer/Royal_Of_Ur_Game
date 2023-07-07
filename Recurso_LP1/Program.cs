3cfa164de1c7e14f6eea6774619894ea5f27a3ce
bread-stealer <guilhermesilvanegrinho@gmail.com>
2023-07-07 19:33:58 +0100

    creating a board on program file

--- Recurso_LP1/Program.cs
+++ Recurso_LP1/Program.cs
@@ -1,9 +1,34 @@
 ﻿using System;
 
-/// <summary>
-/// Represents the entry point of the program.
-/// </summary>
 class Program
 {
+    static void Main()
+    {
+        // Create a 2D array to represent the game board
+        char[,] board =
+        {
+            { '+', '-', '-', '-', '-', '-', '-', '-', '+', ' ', ' ', ' ', '+', '-', '-', '-', '-', '-', '+' },
+            { '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', ' ', ' ', '|', ' ', '|', ' ', '|', ' ', '|' },
+            { '|', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '|' },
+            { '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|' },
+            { '|', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '|' },
+            { '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', ' ', ' ', '|', ' ', '|', ' ', '|', ' ', '|' },
+            { '+', '-', '-', '-', '-', '-', '-', '-', '+', ' ', ' ', ' ', '+', '-', '-', '-', '-', '-', '+' }
+        };
 
+        // Display the game board in the console
+        for (int row = 0; row < board.GetLength(0); row++)
+        {
+            for (int col = 0; col < board.GetLength(1); col++)
+            {
+                Console.Write(board[row, col]);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+
+        // Wait for user input before closing the console window
+        Console.ReadLine();
+    }
 }
+
