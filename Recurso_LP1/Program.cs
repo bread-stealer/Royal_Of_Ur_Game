c3adadd7ed8280d6677a96fb2ebbc4dbf15ed308
bread-stealer <guilhermesilvanegrinho@gmail.com>
2023-07-07 18:55:13 +0100

    Restarting again

--- Recurso_LP1/Program.cs
+++ Recurso_LP1/Program.cs
@@ -5,29 +5,5 @@
 /// </summary>
 class Program
 {
-    /// <summary>
-    /// The main method of the program.
-    /// </summary>
-    /// <param name="args">Command-line arguments.</param>
-    static void Main(string[] args)
-    {
-        GameBoard gameBoard = new GameBoard();
-        gameBoard.DisplayBoard();
 
-        // Create players
-        Player player1 = new Player("Player 1", 'X');
-        Player player2 = new Player("Player 2", 'O');
-
-        gameBoard.SetCellValue(0, 0, player1.Symbol);
-        gameBoard.SetCellValue(1, 3, player2.Symbol);
-        gameBoard.SetCellValue(2, 6, player1.Symbol);
-
-        gameBoard.DisplayBoard();
-
-        // Players make their moves
-        player1.MakeMove();
-        player2.MakeMove();
-
-        Console.ReadLine();
-    }
 }
