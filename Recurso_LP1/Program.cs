1f8d2fe8a730cdd0c0c3a82d3de617aab7f6ccc2
bread-stealer <guilhermesilvanegrinho@gmail.com>
2023-07-07 02:33:44 +0100

    First commit

---
+++ Recurso_LP1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recurso_LP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
