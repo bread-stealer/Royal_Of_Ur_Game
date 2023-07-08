d2fe798b77752727759773eb7ca388ae86dab140
bread-stealer <guilhermesilvanegrinho@gmail.com>
2023-07-08 02:05:52 +0100

    adding xml comments

--- Recurso_LP1/Board.cs
+++ Recurso_LP1/Board.cs
@@ -11,8 +11,16 @@ class GameBoard
         { '|',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ',' ',' ', '|',},
         { '+','-', '-', '-', '+', '-', '-', '-', '-', '-', '-', '+', ' ', ' ', ' ', ' ', '+', '-', '-', '-','-','-', '+',}
     };
-        public const int MaxPosition = 8;
+    /// <summary>
+    /// The maximum position on the game board.
+    /// </summary>
+    public const int MaxPosition = 8;
 
+    /// <summary>
+    /// Displays the game board with player positions.
+    /// </summary>
+    /// <param name="player1">The first player.</param>
+    /// <param name="player2">The second player.</param>
     public static void DisplayBoard(Player player1 = null, Player player2 = null)
     {
         char[,] boardCopy = (char[,])board.Clone();
@@ -39,14 +47,20 @@ class GameBoard
             Console.WriteLine("Player 2 position: " + player2.Position);
     }
 
+    /// <summary>
+    /// Updates the symbol of a player on the game board.
+    /// </summary>
+    /// <param name="board">The game board.</param>
+    /// <param name="player">The player.</param>
+    /// <param name="symbol">The symbol to update.</param>
     private static void UpdatePlayerSymbol(char[,] board, Player player, char symbol)
     {
         int position = player.Position;
 
         if (position == 1)
-            board[1, 11] = symbol; // Update the 'S' position on the board
-        else if (position == MaxPosition) // Check if player reached the end
-            board[1, 17] = symbol; // Update the 'E' position on the board
+            board[1, 11] = symbol;
+        else if (position == MaxPosition)
+            board[1, 17] = symbol;
         else
         {
             int row = GetRow(position);
@@ -56,7 +70,12 @@ class GameBoard
                 board[row, col] = symbol;
         }
     }
-
+    
+    /// <summary>
+    /// Gets the row index on the game board based on the player's position.
+    /// </summary>
+    /// <param name="position">The player's position.</param>
+    /// <returns>The row index.</returns>
     private static int GetRow(int position)
     {
         return position / 3 * 2 + 1;
@@ -74,7 +93,12 @@ class GameBoard
 
         return col;
     }
-
+    /// <summary>
+    /// Checks if the specified row and column indices represent a valid position on the game board.
+    /// </summary>
+    /// <param name="row">The row index.</param>
+    /// <param name="col">The column index.</param>
+    /// <returns>True if the position is valid; otherwise, false.</returns>
     private static bool IsValidPosition(int row, int col)
     {
         return row >= 0 && row < board.GetLength(0) && col >= 0 && col < board.GetLength(1);
--- Recurso_LP1/Program.cs
+++ Recurso_LP1/Program.cs
@@ -1,61 +1,52 @@
 ﻿using System;
 
-class Program
+public class Program
 {
+    /// <summary>
+    /// The main entry point of the program.
+    /// </summary>
     static void Main()
     {
         Console.WriteLine();
         GameBoard.DisplayBoard();
         Console.WriteLine();
 
+        // Prompt for player names
         Console.WriteLine("Enter the name for Player 1:");
         string player1Name = Console.ReadLine();
 
         Console.WriteLine("Enter the name for Player 2:");
         string player2Name = Console.ReadLine();
 
+        // Create player instances
         Player player1 = new Player(player1Name);
         Player player2 = new Player(player2Name);
 
         Console.WriteLine();
         Console.WriteLine("Player 1: " + player1.Name);
         Console.WriteLine("Player 2: " + player2.Name);
+
         Console.WriteLine();
 
         Random random = new Random();
-        int diceNumber;
 
-        do
-        {
-            Console.WriteLine("Press any key to roll the dice for Player 1...");
-            Console.ReadKey();
-            diceNumber = random.Next(1, 7); // Generate a random dice number between 1 and 6
-            Console.WriteLine("Dice Number: " + diceNumber);
-            Console.WriteLine();
+        // Generate a random dice number between 1 and 6
+        int diceNumber = random.Next(1, 7);
 
-            player1.Move(diceNumber, player2);
-            Console.WriteLine(player1.Name + " moved to position: " + player1.Position);
-            Console.WriteLine();
-
-            if (player1.Position == GameBoard.MaxPosition)
-                break;
-
-            Console.WriteLine("Press any key to roll the dice for Player 2...");
-            Console.ReadKey();
-            diceNumber = random.Next(1, 7); // Generate a random dice number between 1 and 6
-            Console.WriteLine("Dice Number: " + diceNumber);
-            Console.WriteLine();
-
-            player2.Move(diceNumber, player1);
-            Console.WriteLine(player2.Name + " moved to position: " + player2.Position);
-            Console.WriteLine();
-
-        } while (player2.Position != GameBoard.MaxPosition);
-
-        Console.WriteLine("Game Over!");
-        Console.WriteLine(player2.Name + " reached the end and won the game!");
+        Console.WriteLine("Dice Number: " + diceNumber);
 
         Console.WriteLine();
+
+        // Move players
+        player1.Move(diceNumber, player2); // Player 1 moves first
+        player2.Move(diceNumber, player1); // Player 2 moves second
+
+        Console.WriteLine(player1.Name + " moved to position: " + player1.Position);
+        Console.WriteLine(player2.Name + " moved to position: " + player2.Position);
+
+        Console.WriteLine();
+
+        // Display the updated game board
         GameBoard.DisplayBoard(player1, player2);
         Console.WriteLine();
     }
